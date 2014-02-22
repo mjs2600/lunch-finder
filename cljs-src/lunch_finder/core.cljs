d7f8e1fce1f182d8bc0e3e151ab349245df50b9b
Michael Simpson <mjs2600@gmail.com>
2014-02-22 13:44:05 -0500

    Limit a user to 5 votes

--- cljs-src/lunch_finder/core.cljs
+++ cljs-src/lunch_finder/core.cljs
@@ -2,17 +2,30 @@
   (:require-macros [cljs.core.async.macros :refer [go]])
   (:require [om.core :as om :include-macros true]
             [om.dom :as dom :include-macros true]
-            [cljs.core.async :refer [put! chan <!]]))
+            [cljs.core.async :refer [put! chan <!]]
+            [clojure.data :as data]
+            [clojure.string :as string]))
 
 (enable-console-print!)
 
 (def app-state (atom {:restaurants [{:name "Q-Shack" :votes 0}
-                                    {:name "Thai Cafe" :votes 0}]}))
+                                    {:name "Thai Cafe" :votes 0}]
+                      :user {:votes 5}}))
 
-(defn vote-for-restaurant [restaurants vote]
-  (vec (map #(if (= % vote)
-             (update-in % [:votes] inc)
-             %) restaurants)))
+(defn vote-for-restaurant [app vote]
+  (let [restaurants (:restaurants app)
+        user (:user app)]
+    (assoc app :restaurants
+           (vec (map #(if (= % vote)
+                        (update-in % [:votes] inc)
+                        %) restaurants)))))
+
+(defn cast-vote [app vote]
+  (let [remaining-votes (get-in app [:user :votes])]
+    (if (> remaining-votes 0)
+      (vote-for-restaurant (update-in app [:user :votes] dec)
+                           vote)
+      app)))
 
 (defn restaurant-view [restaurant owner]
   (reify
@@ -33,8 +46,8 @@
       (let [vote (om/get-state owner :vote)]
         (go (loop []
               (let [restaurant (<! vote)]
-                (om/transact! app :restaurants
-                              (fn [xs] (vote-for-restaurant xs restaurant)))
+                (om/transact! app
+                              (fn [xs] (cast-vote xs restaurant)))
                 (recur))))))
     om/IRenderState
     (render-state [this {:keys [vote]}]
@@ -43,6 +56,7 @@
                (apply dom/dl #js{:className "restaurants"}
                       (om/build-all restaurant-view (:restaurants app)
                                     {:init-state {:vote vote}}))))))
+
 
 (om/root
  restaurants-view
